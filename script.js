610b5571f1807f1e018dbcab829cb7e8b37153f6
Alenso Lopes <alenso.lopes@ecole.ensicaen.fr>
2026-09-10 11:21:25 +0200

    feat: ensicaen support and manifest for firefox

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const url = 'https://ecampus.unicaen.fr/'
+const fileData = ["https://ecampus.unicaen.fr", "https://foad.ensicaen.fr"];
 
 setInterval(() => {
-  if(!window.location.href.startsWith(url)) 
-    return;
-  document.querySelectorAll('a')
-    .forEach(a => {
-      if(a.href.includes('forcedownload=1')){
-        a.href = a.href.replace('forcedownload=1', '');
-        a.target = '_blank';
-      }
-    });
-}, 1000);
+  fileData.forEach(url => {
+    if (!window.location.href.startsWith(url))
+      return;
+    document.querySelectorAll('a')
+      .forEach(a => {
+        if (a.href.includes('forcedownload=1')) {
+          a.href = a.href.replace('forcedownload=1', '');
+          a.target = '_blank';
+        }
+      });
+  });
+}, 250);
